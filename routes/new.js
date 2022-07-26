f1f21465dfe2d4c43f444ddab5af5fee33a63c1d
rahulsingh34 <rahulsingh342000@gmail.com>
2022-07-26 19:56:13 -0400

    added form route

--- routes/new.js
+++ routes/new.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET new form. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+	res.render('form');
+  });
+  
+  module.exports = router;
