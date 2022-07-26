cbd6b2f3cb618b4c2fe8dcd829bf2a890422176e
rahulsingh34 <rahulsingh342000@gmail.com>
2022-07-26 19:25:21 -0400

    display messages on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+//Messages array
+const messages = [
+	{
+	  text: "Hi there!",
+	  user: "Amando",
+	  added: new Date().toLocaleDateString("en-US")
+	},
+	{
+	  text: "Hello World!",
+	  user: "Charles",
+	  added: new Date().toLocaleDateString("en-US")
+	}
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
 module.exports = router;
